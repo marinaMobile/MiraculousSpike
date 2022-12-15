1b9e8af65c4cf3143dd9f9912d2fbc41d9933014
gaaats <mr.apemi@gmail.com>
2022-12-15 11:00:14 +0200

    done_vithout_icons

--- app/src/main/java/com/aqupepgames/projectp/one/OneGame.kt
+++ app/src/main/java/com/aqupepgames/projectp/one/OneGame.kt
@@ -3,6 +3,7 @@ package com.aqupepgames.projectp.one
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aqupepgames.projectp.R
+import kotlin.random.Random
 
 class OneGame : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,6 @@ class OneGame : AppCompatActivity() {
     }
 
     companion object{
-        var currentBalance = 5000
+        var currentBalance = Random.nextInt(from = 3000, until = 10000)
     }
 }
