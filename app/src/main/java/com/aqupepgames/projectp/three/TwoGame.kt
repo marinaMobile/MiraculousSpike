76db14c0c824e32ac40a1ec940f5547304ffa078
Marina <marina@Marinas-MacBook-Pro.local>
2022-12-16 16:34:53 +0300

    aa

--- app/src/main/java/com/aqupepgames/projectp/three/TwoGame.kt
+++ app/src/main/java/com/aqupepgames/projectp/three/TwoGame.kt
@@ -1,26 +1,10 @@
 package com.aqupepgames.projectp.three
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
-import android.content.Context
-import android.graphics.drawable.AnimationDrawable
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.GestureDetector
-import android.view.Gravity
-import android.view.MotionEvent
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.view.GestureDetectorCompat
-import com.aqupepgames.projectp.R
 import com.aqupepgames.projectp.databinding.ActivityTwoGameBinding
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
-import kotlin.random.Random
+
 
 class TwoGame : AppCompatActivity() {
 
